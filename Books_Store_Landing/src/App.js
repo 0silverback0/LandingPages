6977c86dbf9db4b0e596865ef146306522bc45ae
Bharath-KumarReddy <bk1726797@gmail.com>
2023-10-20 11:35:58 +0530

    Books store LandingPage

---
+++ Books_Store_Landing/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter , Routes,  Route } from "react-router-dom";
+import Home from './components/Home';
+
+function App() {
+  return (
+    
+    <>
+
+    <BrowserRouter>
+    <Routes>
+      <Route path="/"  element={<Home/>} />
+    </Routes>
+    </BrowserRouter>
+    </>
+    
+  );
+}
+
+export default App;
